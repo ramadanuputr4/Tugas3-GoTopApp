acca60f33c782ac7e327162d8c3af17fc4ac83a9
ramadanuputr4 <ramadanu.putra11@gmail.com>
2021-04-03 12:44:09 +0700

    GoTopApp

---
+++ app/src/main/java/com/latihan/gotopapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.latihan.gotopapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tampilan_menu);
+    }
+
+    public void clickRS(View view) {
+        //Gambar Rumah Sakit di Click
+        Intent i = new Intent(this,RS.class);
+        startActivity(i);
+
+    }
+}
